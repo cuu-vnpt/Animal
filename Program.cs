27d67ae51c8af704803a53ceccbb3411f6317856
cuu-vnpt <kunnobita97@gmail.com>
2024-01-14 09:47:23 +0700

    newfile

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cat mycat = new Cat("40","165");
+    }
+}
+    public abstract class Animal
+    {
+        protected string weight{get;set;}
+        
+        protected string height{get;set;}
+
+        public Animal(string weight, string height)
+        {
+            this.weight = weight;
+            this.height = height;
+        }
+        public virtual void PrintInfo()
+        {
+
+        }
+
+    }
+    public class Cat : Animal 
+    {
+        public Cat(string weight, string height) : base(weight,height)
+    {
+
+    }
+    }
